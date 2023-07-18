35f7469d4b9aed1bfe201ac0b8ed7eff7ab55c52
zachdenardi <zdenardi@gmail.com>
2023-07-18 11:35:32 -0700

    Got the logic for mouseover and mouseleave

--- injected.js
+++ injected.js
@@ -1,4 +1,6 @@
+
 $(async()=>{
+    let freeze = false;
     /**
      * Delays for X amount of milliseconds
      * @param ms
@@ -15,20 +17,39 @@ $(async()=>{
     const fifthDiv = $('#5')
 
     const divs = [firstDiv,secondDiv,thirdDiv,fourthDiv,fifthDiv]
+
+    const removeAllActives = ()=>{
+        $("#1,#2,#3,#4,#5").removeClass('active')
+    }
     let i = 0
-    while(i <= 4){
-        if (i===4){
-            console.log("done")
-            i=0
-        }else{
-            divs[i].addClass('active')
-            await delay(1000)
-            divs[i].removeClass('active')
-            i++
+    const cycleThroughDivs = async (i)=>{
+        while(i <= 5){
+            if (i===5){
+                console.log("done")
+                i=0
+            }else if(!freeze){
+                $(".active").removeClass('active')
+                divs[i].addClass('active')
+                await delay(5000)
+                i++
+            }else{
+                console.log("frozen")
+                break
+            }
         }
-
-
     }
 
+    $("#1,#2,#3,#4,#5").on("mouseover",(div)=>{
+        $('.active').removeClass('active')
+        $(div.currentTarget).addClass('active')
+        freeze = true;
+    })
+    $("#1,#2,#3,#4,#5").on("mouseleave",async (div)=>{
+        freeze = false
+        i = div.target.id
+        await cycleThroughDivs(i)
+    })
+    await cycleThroughDivs(i)
 
 })
+
